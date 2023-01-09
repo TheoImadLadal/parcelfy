f1ad0c4afc74c2ffa314b9899380ddf99af8b48f
Theo Imad Ladal <120195375+TheoImadLadal@users.noreply.github.com>
2023-01-09 15:23:11 +0100

    Update Program.cs

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -19,12 +19,8 @@ builder.Services.AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
 app.UseSwagger();
 app.UseSwaggerUI();
-}
-
 
 app.UseHttpsRedirection();
 
